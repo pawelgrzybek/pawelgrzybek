c0435b05ddf92280a0f126750ecbbb6f8d442227
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2020-08-16 21:59:09 +0100

    Update get date logic

--- index.js
+++ index.js
@@ -9,36 +9,16 @@ const DESCRIPTION =
   "I am a self-taught front-end developer looking for an opportunity to work with the latest web technologies. Coming from a background in photography, I have progressed through UI/UX design into front-end development with a growing interest in other bleeding edge technologies. Over time I have become strongly involved in the open-source community where I regularly contribute and create projects, frequently attend conferences and follow other liked-minded geeks. This enables me to always stay fresh in finding the best solutions for technical problems. When I’m not learning or working on another blog post, I indulge my passion for jazz and funk music that helps me to maintain a balance between my virtual and real life.";
 const TITLE_MOST_RECENT_POSTS = "Recent blog posts";
 
-// const exec = (cmd, args = []) =>
-//   new Promise((resolve, reject) => {
-//     const app = spawn(cmd, args, { stdio: "inherit" });
-//     app.on("close", (code) => {
-//       if (code !== 0) {
-//         err = new Error(`Invalid status code: ${code}`);
-//         err.code = code;
-//         return reject(err);
-//       }
-//       return resolve(code);
-//     });
-//     app.on("error", reject);
-//   });
+const pad = (v) => v.toString().padStart(2, "0");
 
-// const commitFile = async () => {
-//   await exec("git", [
-//     "config",
-//     "--global",
-//     "user.email",
-//     "readme-bot@example.com",
-//   ]);
-//   await exec("git", ["config", "--global", "user.name", "readme-bot"]);
-//   await exec("git", ["add", "README.md"]);
-//   await exec("git", [
-//     "commit",
-//     "-m",
-//     ":zap: update readme with the recent activity",
-//   ]);
-//   await exec("git", ["push"]);
-// };
+const getDateFormatted = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+
+  return `${year}.${pad(month)}.${pad(day)}`;
+};
 
 const fetchArticles = async () => {
   const articles = await fetch(FEED_URL);
@@ -51,7 +31,7 @@ const fetchArticles = async () => {
 
 async function main() {
   const posts = await fetchArticles();
-  console.log(posts);
+
   const readme = `
 # ${TITLE}
 
@@ -61,11 +41,14 @@ ${DESCRIPTION}
 
 ## ${TITLE_MOST_RECENT_POSTS}
 
+<!-- FEED-START -->
+<!-- FEED-END -->
+
 ${posts}
 
 Visit [https://pawelgrzybek.com](https://pawelgrzybek.com) to read more.
 
-Last update: ${new Date()}
+Last update: ${getDateFormatted()}
 
 `;
 
